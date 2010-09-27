cf68e2263a595f2d4a1f4c90a1e6649243e86915
Will Donnelly <wgd@willdonnelly.net>
2010-09-26 22:42:16 -0500

    It kinda does writes now I think

--- swd.py
+++ swd.py
@@ -30,26 +30,16 @@ class BusPirate:
 
     def bytes (self, count):
         self.port.write(bytearray([0x06] * count))
-        return bytearray([self.port.read(1) for x in range(count)])
+        return [self.port.read(1) for x in range(count)]
 
-# put the STM32 chip into SWD mode by sending the correct bitstream
-def swdEntry(pirate):
-    pirate.send([0xFF] * 8)
-    pirate.send([0x79,0xE7])
-    swdReset(pirate)
+def bitCount(int_type):
+    count = 0
+    while(int_type):
+        int_type &= int_type - 1
+        count += 1
+    return(count)
 
-# send a bit pattern which fixes any SWD framing issues
-def swdReset(pirate):
-    pirate.send([0xFF] * 8)
-    pirate.send([0x00] * 8)
-
-def calcOpcode(ap=False, register=0x00, read=True):
-    def bitCount(int_type):
-        count = 0
-        while(int_type):
-            int_type &= int_type - 1
-            count += 1
-        return(count)
+def calcOpcode (ap=False, register=0x00, read=True):
     opcode = 0x00
     opcode = opcode | (0x20 if read else 0x00)
     opcode = opcode | (0x40 if ap else 0x00)
@@ -58,21 +48,56 @@ def calcOpcode(ap=False, register=0x00, read=True):
     opcode = opcode | 0x81
     return opcode
 
-def swdRead(pirate, ap=False, register=0x00):
-    pirate.send([calcOpcode(ap, register, True)])
-    ack = pirate.bits(3)
-    data = pirate.bytes(4)
-    extra = pirate.bits(3)
-    return data
+class SWDP:
+    def __init__ (self, f = "/dev/bus_pirate"):
+        self.pirate = BusPirate(f)
+        self.pirate.send([0xFF] * 8)
+        self.pirate.send([0x79,0xE7])
+        self.resync()
+
+    def resync (self):
+        self.pirate.send([0xFF] * 8)
+        self.pirate.send([0x00] * 8)
+
+    def read (self, ap = False, register = 0x00):
+        self.pirate.send([calcOpcode(ap, register, True)])
+        ack = self.pirate.bits(3)
+        if ack[0:3] != ['\x01', '\x00', '\x00']:
+            print("error in SWD stream")
+            print(ack[0:3])
+            sys.exit(1)
+        data = self.pirate.bytes(4)
+        extra = self.pirate.bits(3)
+        if sum([bitCount(ord(x)) for x in data[0:3]]) % 2 != ord(extra[0]):
+            print("parity error")
+        return data
+
+    def write (self, data, ap = False, register = 0x00):
+        self.pirate.send([calcOpcode(ap, register, False)])
+        ack = self.pirate.bits(5)
+        if ack[0:3] != ['\x01', '\x00', '\x00']:
+            print("error in SWD stream")
+            print(ack[0:3])
+            sys.exit(1)
+        self.pirate.send(data[0:4])
+        if sum([bitCount(x) for x in data[0:4]]) % 2:
+            self.pirate.port.write(bytearray([0x0D, 0x09]))
+            self.pirate.port.read(2)
+        else:
+            self.pirate.port.write(bytearray([0x0C, 0x09]))
+            self.pirate.port.read(2)
 
 def main():
-    pirate = BusPirate("/dev/ttyUSB0")
-    swdEntry(pirate)
-    swdReset(pirate)
-    print(hexEncode(swdRead(pirate, register=0x00)))
+    swdp = SWDP("/dev/ttyUSB0")
+    swdp.resync()
+    print hexEncode( swdp.read(ap = False, register = 0b00) )
+    for i in range(5):
+        swdp.write([0x3F, 0x00, 0x00, 0x00], ap = False, register = 0b01)
+        swdp.write([0x3F, 0x00, 0x00, 0x00], ap = False, register = 0b10)
+        print hexEncode( swdp.read(ap = True, register = 0b00) )
 
-def hexEncode (ba):
-    return ''.join(["%02X " % b for b in ba]).strip()
+def hexEncode (bs):
+    return ''.join(["%02X " % b for b in bytearray(bs)]).strip()
 
 if __name__ == "__main__":
     main()
